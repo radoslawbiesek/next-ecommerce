c4cfd11f544bade2b013c87fd3cd53c3105b3d4b
Radosław Biesek <radoslaw.biesek@gmail.com>
2024-02-07 20:05:03 +0100

    configure tools

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 120,
+  useTabs: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+};
