95ed17627a8c16d8925c0fc4fd294d122e9f23c1
Radosław Biesek <radoslaw.biesek@gmail.com>
2024-03-24 13:56:20 +0100

    simplify webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,18 +1,6 @@
-import { type NextRequest } from "next/server";
-import { headers } from "next/headers";
 import { revalidatePath } from "next/cache";
 
-export async function POST(request: NextRequest): Promise<Response> {
-  const secret = headers().get("x-webhook-secret");
-  if (secret !== process.env.WEBHOOK_SECRET) {
-    return new Response("Unauthorized", { status: 401 });
-  }
-
-  const body: unknown = await request.json();
-  if (typeof body !== "object" || body === null) {
-    return new Response("Bad Request", { status: 400 });
-  }
-
+export async function POST(): Promise<Response> {
   revalidatePath("products/[pageNumber]");
   revalidatePath("product/[slug]");
   revalidatePath("categories/[slug]/[pageNumber]");
